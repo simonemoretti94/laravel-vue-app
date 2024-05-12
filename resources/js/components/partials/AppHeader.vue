89cef844f5493e3d1de662ec95b1cb53ef209771
simonemoretti94 <simone.moretti.944@gmail.com>
2024-05-13 00:56:22 +0200

    2th added table

--- resources/js/components/partials/AppHeader.vue
+++ resources/js/components/partials/AppHeader.vue
@@ -7,9 +7,9 @@ export default {
 <template>
     <header class="bg-dark bg-gradient ">
         <nav class="nav justify-content-center  ">
-            <a class="nav-link active" href="#" aria-current="page">Active link</a>
-            <a class="nav-link" href="#">Link</a>
-            <a class="nav-link active" href="/index">Disabled link</a>
+            <a class="nav-link active" href="#" aria-current="page">Home</a>
+            <a class="nav-link" href="#">About us</a>
+            <a class="nav-link active" href="/index">Contacts</a>
         </nav>
     </header>
 </template>
--- resources/js/components/partials/MainContent.vue
+++ resources/js/components/partials/MainContent.vue
@@ -18,6 +18,29 @@ export default {
     <div class="cowntainer">
         <h1 class="m-1 border border-1 text-center">{{ info }} to my vue {{ state.greetings }}!</h1>
         <div class="row">
+            <div class="table-responsive-lg mt-2 px-2">
+                <table class="table table-secondary">
+                    <thead>
+                        <tr>
+                            <th scope="col">Column 1</th>
+                            <th scope="col">Column 2</th>
+                            <th scope="col">Column 3</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="">
+                            <td scope="row">R1C1</td>
+                            <td>R1C2</td>
+                            <td>R1C3</td>
+                        </tr>
+                        <tr class="">
+                            <td scope="row">Item</td>
+                            <td>Item</td>
+                            <td>Item</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
             <div class="col-xl-2 col-md-6 col-sm-6 p-1">
                 <div>
                     <p>lorem top</p>
