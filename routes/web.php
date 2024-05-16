dafd46896a1135883a7f4b6dbf09497bad9fb915
simonemoretti94 <simone.moretti.944@gmail.com>
2024-05-16 19:12:08 +0200

    5.2.3

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\PostController;
-use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,10 +8,3 @@ Route::get('/', function () {
 });
 
 Route::resource('posts', PostController::class);
-
-// Route::get('/admin/posts', [PostController::class, 'index'])->name('admin.posts.index');
-// Route::get('/admin/posts/create', [PostController::class, 'create'])->name('admin.posts.create');
-// Route::post('/admin/posts', [PostController::class, 'store'])->name('admin.posts.store');
-// Route::get('/admin/posts/{Post}/edit', [PostController::class, 'edit'])->name('admin.posts.edit');
-// Route::put('/admin/posts/{Post}', [PostController::class, 'update'])->name('admin.posts.update');
-// Route::delete('/admin/posts/{Post}', [PostController::class, 'destroy'])->name('admin.posts.destroy');
